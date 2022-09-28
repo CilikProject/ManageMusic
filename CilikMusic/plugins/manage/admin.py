4f72c70f5499a8d94d2cb0e7431f6871cd619a46
CilikProject <114132441+CilikProject@users.noreply.github.com>
2022-09-29 04:38:13 +0700

    Update admin.py

--- CilikMusic/plugins/manage/admin.py
+++ CilikMusic/plugins/manage/admin.py
@@ -3,31 +3,42 @@ from pyrogram import filters
 from config import BANNED_USERS
 from CilikMusic import app
 from CilikMusic.utils.misc import extract_user
-from CilikMusic.utils.decorators import AdminActual
+from CilikMusic.utils.decorators import AdminRightsCheck
 from pyrogram.errors import ChatAdminRequired
 from pyrogram.types import ChatPermissions, Message
 
+
 @app.on_message(
-    filters.command("pm", ["/", "."])
+    filters.command("pm")
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
 )
-@AdminActual
-async def promote(message: Message):
-    yanto = message.reply_to_message.from_user.id 
-    await app.promote_chat_member(message.chat.id,
-    user_id=yanto,
-        can_change_info=True,
-        can_invite_users=True,
-        can_delete_messages=True,
-        can_restrict_members=True,
-        can_pin_messages=True,
-        can_promote_members=True,
-        can_manage_chat=True,
-        can_manage_voice_chats=True,
-    )
-    await message.reply_text("Promoted Succes")
+@AdminRightsCheck
+async def promote(cli, message: Message, _, chat_id):
+    if not message.reply_to_message:
+        if len(message.command) != 2:
+            return await message.reply_text(_["general_1"])
+        user = message.text.split(None, 1)[1]
+        if "@" in user:
+            user = user.replace("@", "")
+        elif message.reply_to_message.from_user.id
+            user = message.reply_to_message.from_user.id
+    try:
+        await app.promote_chat_member(message.chat.id,
+            user_id=user,
+            can_change_info=True,
+            can_invite_users=True,
+            can_delete_messages=True,
+            can_restrict_members=True,
+            can_pin_messages=True,
+            can_promote_members=True,
+            can_manage_chat=True,
+            can_manage_voice_chats=True,
+        )
+        xx = await message.reply_text("Promoted Succes")  
+    except ChatAdminRequired:
+        return await xx.edit("**Maaf Anda Bukan admin**")   
     
   
 @app.on_message(filters.command("dm", [".", "^", "-", "!", "/"]))
